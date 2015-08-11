3626a91e27c80b0649dd1f5418ca49d707f12069
Scott Seligman <info@tinyantelope.com>
2015-08-10 21:14:56 -0400

    Initial Commit

---
+++ Plate Calculator 9000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plate Calculator 9000
+//
+//  Created by Philip Seligman on 8/10/15.
+//  Copyright (c) 2015 tiny antelope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
